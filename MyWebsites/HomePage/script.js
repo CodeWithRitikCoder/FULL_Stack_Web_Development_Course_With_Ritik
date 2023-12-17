0ffe83a909c29a9ba26a6905d1fa72791a1a6535
Ritik verma <132076091+CodeWithRitikCoder@users.noreply.github.com>
2023-12-18 05:22:13 +0530

    I have updated a lots of thing in MyWebsite.

--- MyWebsites/HomePage/script.js
+++ MyWebsites/HomePage/script.js
@@ -1,1 +1,9 @@
 // alert("Welcome to My New Website")
+// const navLinkEls = document.querySelectorAll('header');
+// const windowPathname = window.location.pathname;
+
+// navLinkEls.forEach(navLinkEl => {
+//     if(navLinkEl.href.includes(windowPathname)) {
+//         navLinkEl.classList.add('active');
+//     }
+// });
